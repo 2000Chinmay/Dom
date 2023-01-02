364b3cc04361f136c01682aaf9882ec9fc658c76
2000Chinmay <Chinmay.deolekar1@gmail.com>
2023-01-02 19:31:46 +0530

    First Commit

---
+++ dom.js
@@ -0,0 +1,14 @@
+var header_title = document.getElementById('header-title');
+var Header = document.getElementById('main-header');
+//console.log(Header);
+//Header.textContent = 'Hello';
+//Header.innerText = 'GoodBye';
+//console.log(Header.textContent);
+//console.log(Header.innerText);
+//header_title.style.padding = '30px 30px 30px 30px';
+//header_title.style.border = 'solid 1px #000';
+//Header.style.borderBottom = 'solid 3px #000';
+var select = document.getElementsByClassName('title');
+console.log(select[0]);
+select[0].style.fontWeight = 'bold';
+select[0].style.color = 'green';
