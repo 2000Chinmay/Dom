1eaa5163d6115dea646537e37660a4f94e9c73ec
2000Chinmay <Chinmay.deolekar1@gmail.com>
2023-01-02 19:56:07 +0530

    Second commit

--- dom.js
+++ dom.js
@@ -8,7 +8,11 @@ var Header = document.getElementById('main-header');
 //header_title.style.padding = '30px 30px 30px 30px';
 //header_title.style.border = 'solid 1px #000';
 //Header.style.borderBottom = 'solid 3px #000';
-var select = document.getElementsByClassName('title');
-console.log(select[0]);
-select[0].style.fontWeight = 'bold';
-select[0].style.color = 'green';
+var select = document.getElementsByClassName('list-group-item');
+console.log(select[2]);
+select[2].style.backgroundColor = 'green';
+
+for(var i= 0; i< select.length; i++){
+    select[i].style.fontWeight = 'bold';
+}
+
